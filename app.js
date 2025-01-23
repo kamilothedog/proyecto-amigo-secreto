e7caed39d5d824bb8dba89bf3e6ce41f4067b40e
kamilothedog <osva1178@gmail.com>
2025-01-23 14:34:05 -0600

    agregando elementos a la lista HTML

--- app.js
+++ app.js
@@ -13,5 +13,23 @@ function agregarAmigo() {
     amigos.push(nombreAmigo);
     // Limpia el campo de texto
     document.getElementById('amigo').value = "";
+    agregarArregloEnLista(amigos);
     console.log(amigos);
+}
+
+function agregarArregloEnLista(arreglo){
+    const lista = document.getElementById('listaAmigos'); // Obtiene el elemento <ul> de la lista
+    lista.innerHTML = ""; // Limpia la lista
+    // Otra forma de hacerlo sin el ciclo for
+    /*arreglo.forEach((elemento) => {
+        const li = document.createElement('li'); // Crea un elemento <li>
+        li.textContent = elemento; // Agrega el texto al <li>
+        lista.appendChild(li); // Agrega el <li> a la lista
+    });
+    */
+   for(let i=0; i<arreglo.length; i++){
+    const li = document.createElement('li'); // Crea un elemento <li>
+    li.textContent = arreglo[i]; // Agrega el texto al <li>
+    lista.appendChild(li); // Agrega el <li> a la lista
+   }
 }
